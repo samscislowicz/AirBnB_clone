6d3c73b19dc960a40a1fd06e55a0597c61bd1f74
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-15 22:31:18 +0000

    spelled make correctly this time

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,16 +3,19 @@
 from fabric.api import *
 from datetime import datetime
 import os
+import tarfile
 
+env.user = "ubuntu"
 
 def do_pack():
-    date = datetime()
+    """Creates a tgz achive of the directory"""
+    date = (datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
     name = "versions/web_static_{}.tgz".format(date)
 
-    if not os.path.exists("./versions"):
-        os.mkdirs("./versions")
+    if not os.path.exists("./versions/"):
+        os.makedirs("./versions/")
     try:
         local("tar -cvzf {} web_static".format(name))
         return (name)
     except:
-        return None
+        return (None)
